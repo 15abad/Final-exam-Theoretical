7331017ffb96e0483659e8b0a7253087b438744f
15abad <122170037+15abad@users.noreply.github.com>
2023-01-11 15:30:23 +0800

    Create costumer.java

---
+++ costumer.java
@@ -0,0 +1,37 @@
+public abstract class Customer 
+ { 
+     protected String name; 
+     protected double amount; 
+          
+         Customer(String name, double amount){ 
+                 this.name = name; 
+                 this.amount = amount; 
+         } 
+          
+         public void setName(String name){ 
+                 this.name = name; 
+         } 
+          
+         public String getName(){ 
+                 return name; 
+         } 
+           
+         public void setAmount(double amount){ 
+                 this.amount = amount; 
+         } 
+  
+         public double getAmount(){ 
+                 return amount; 
+         } 
+          
+         abstract double calculateBill(); 
+ }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
